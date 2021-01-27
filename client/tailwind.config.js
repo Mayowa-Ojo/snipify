098e5f359078746e46437ae25b3c154e8a836d9e
Mayowa Ojo <mayowaojo.e@gmail.com>
2021-01-26 23:36:40 -0700

    :wrench: [config]: setup tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+   purge: [],
+   theme: {
+      extend: {
+         fontSize: {
+            "12": ".75rem",
+            "13": ".81rem",
+            "14": ".88rem",
+            "15": ".94rem",
+            "18": "1.13rem",
+            "21": "1.31rem",
+            "24": "1.5rem",
+            "52": "3.25rem"
+         },
+         borderOpacity: {
+            "10": "0.1"
+         }
+      },
+   },
+   variants: {
+      textColor: ["responsive", "hover", "focus", "group-hover", "group-focus"],
+      visibility: ["responsive", "hover", "focus", "group-hover", "group-focus"]
+   },
+   plugins: [],
+}
